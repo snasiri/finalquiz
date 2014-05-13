cc0ed001d1b06081458db981a5687b7cc9f06b5c
snasiri <snasiri1@alladins-air.home>
2014-05-13 12:49:08 -0400

    Prepare for exam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Finalquiz::Application.config.secret_key_base = '2f66e14a1d05ba2f2874d958466f1bcd84ab91a81a3b79dafc380cb03e1d9bffa2d25fe494268cfdf723f15f7c316d0ba730264e8fb94199be902760d276475c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Finalquiz::Application.config.session_store :cookie_store, key: '_finalquiz_session'
